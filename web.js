7f0cfb2900e96377162df279b0ff68e2d7457414
saurabh <saurabhkb123@gmail.com>
2012-08-21 23:26:07 +0530

    update index page

--- web.js
+++ web.js
@@ -1,9 +1,26 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.configure(function(){
+app.use(stylus.middleware({
+                src: __dirname + '/public',
+                compile: function(str, path){
+                                return stylus(str).set('filename', path).set('compress', true).use(nib());
+                        }
+        }));
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade');
+        app.set('view options', {layout: false});
+        app.use(express.bodyParser({keepExtensions: true, uploadDir: __dirname + '/public/uploads'}));
+        app.use(express.cookieParser());
+        app.use(express.session({store: sessionStore, secret: "keyboard cat", key: "express.sid"}));
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(__dirname + '/public'));
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render("index.jade", {title: "Teachfruit");
   });
 
   var port = process.env.PORT || 5000;
