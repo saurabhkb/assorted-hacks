afa48768d13a91e8a7ea539dcf9ca6bf13e47c50
saurabh <saurabhkb123@gmail.com>
2012-09-04 10:48:57 +0530

    replaced LIKE with ~* for regex  matching

--- routes/index.js
+++ routes/index.js
@@ -362,8 +362,8 @@ exports.extraoptions = function(req, res){
 exports.searchpage = function(req, res){
 	var qu = req.query['q'];
 	var course_results = '';
-	var cq = "SELECT * FROM courses WHERE course_name LIKE '[[:<:]]" + qu + "' LIMIT 50";
-	var uq = 'SELECT * FROM "user" WHERE first_name LIKE \'[[:<:]]' + qu + '\' OR last_name LIKE \'[[:<:]]' + qu + '\' LIMIT 50';
+	var cq = "SELECT * FROM courses WHERE course_name ~* '[[:<:]]" + qu + "' LIMIT 50";
+	var uq = 'SELECT * FROM "user" WHERE first_name ~* \'[[:<:]]' + qu + '\' OR last_name ~* \'[[:<:]]' + qu + '\' LIMIT 50';
 	pg.connect(conString, function(err, client){
 		if(err) throw err;
 		client.query(cq, function(err, result){
