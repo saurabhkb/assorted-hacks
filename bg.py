b4ef403a874acb3c6c52c0325ddf397dc14580d5
saurabh <saurabhkb123@gmail.com>
2013-06-21 11:49:27 +0530

    changed starting point to android devices

--- bg.py
+++ bg.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from crawler import Crawler
 c = Crawler()
-c.traverse('Information_technology_companies_of_the_United_States')
+c.traverse('Android_(operating_system)_devices')
+#c.traverse('Information_technology_companies_of_the_United_States')
 #c.traverse('Video_game_companies_of_the_United_States')
