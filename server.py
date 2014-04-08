63f5286c387e46885359c92400c91780300bd0c7
saurabh <saurabhkb123@gmail.com>
2014-04-08 11:32:21 +0530

    added backend mongodb query logic and routing

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, abort
+import time
 from pymongo import MongoClient
 import json
 
@@ -11,10 +12,27 @@ v = db['vm']
 def home():
 	return render_template("index.html")
 
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
+
+@app.route('/provider/<p>')
+def provider(p):
+	if p == "Amazon":
+		return redirect("http://aws.amazon.com/")
+	elif p == "Azure":
+		return redirect("http://azure.microsoft.com/en-us/")
+	else:
+		return abort(404)
+
 @app.route('/fetch_results', methods = ['POST', 'GET'])
 def fetch():
-	page_num = int(request.form['page'])
-	assert page_num > 0
+	#page_num = int(request.form['page'])
+	#assert page_num > 0
 
 	cores_min = int(request.form['cores_min'])
 	cores_max = int(request.form['cores_max'])
@@ -28,9 +46,36 @@ def fetch():
 	ssd_min = int(request.form['ssd_min']) * 10 ** 6
 	ssd_max = int(request.form['ssd_max']) * 10 ** 6
 
-	q = {"cores": {"$gte": cores_min, "$lte": cores_max}, "ramMB": {"$gte": memory_min, "$lte": memory_max}, "diskMB": {"$gte": disk_min, "$lte": disk_max}, "flashMB": {"$gte": ssd_min, "$lte": ssd_max}}
+	q = {
+		"cores": {
+			"$gte": cores_min,
+			"$lte": cores_max
+		},
+		"ramMB": {
+			"$gte": memory_min,
+			"$lte": memory_max
+		},
+		"diskMB": {
+			"$gte": disk_min,
+			"$lte": disk_max
+		},
+		"flashMB": {
+			"$gte": ssd_min,
+			"$lte": ssd_max
+		}
+	}
 	result = list(v.find(q, {'_id': False}))
+	for r in result:
+		r['provider'] = "<a target='_blank' href='/provider/%s'>%s</a>" % (r['provider'], r['provider'])
+		r['ramGB'] = r['ramMB'] / 1000.0
+		r['diskGB'] = r['diskMB'] / 1000.0
+		r['flashGB'] = r['flashMB'] / 1000.0
+
+		del r['ramMB']
+		del r['diskMB']
+		del r['flashMB']
 	#return jsonify(length = len(result), result = result[page_num : page_num + 10])
+	time.sleep(2)
 	return jsonify(length = len(result), result = result)
 
 app.run(debug = True)
